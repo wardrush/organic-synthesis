28c1880505c1c858c8eeefcfe786142971c6de43
Gawquon <Gawquon@users.noreply.github.com>
2016-12-19 04:26:37 -0600

    Add files via upload

---
+++ bondList.h
@@ -0,0 +1,43 @@
+//
+// Created by Michael on 12/16/2016.
+//
+
+#ifndef ORGOMATIC_BONDLIST_H
+#define ORGOMATIC_BONDLIST_H
+
+#include <string>
+#include <vector>
+
+//custom error
+struct noSuchBond:std::exception{
+    char const *what() const noexcept{
+        return "That atom had no bonds";
+    }
+};
+
+class bondList
+{
+private:
+    struct Node
+    {
+        size_t pos;
+        size_t bVal; // bond value, 1 = single bond, 2 = double bond
+        Node *next;
+    };
+    typedef Node *NodePtr;
+
+    std::vector<NodePtr> central; //A list of head pointers representing each atom
+public:
+    bondList();
+    bondList(size_t size);
+    void addPoint();
+    void formBond(size_t posC, size_t con); //posC is the atom to add the bond to, con is the other atom to connect to
+    void formBond(size_t posC, size_t con, size_t bondValue);
+    void removePoint(size_t posC);
+    void breakBond(size_t posC, size_t con);
+    void alterBondValue(size_t posC, size_t con, size_t val); //val is the new value to change to
+    size_t checkHybrid(size_t pos);
+};
+
+
+#endif //ORGOMATIC_BONDLIST_H
