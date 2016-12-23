e6b7b7b87aaab5768f3c1b21e93af3cc8e2d877d
wardrush <wardrushton@gmail.com>
2016-12-23 10:10:54 -0600

    Added MolarMass.cpp
    Very preliminary skeleton of csv parser

---
+++ MolarMass.cpp
@@ -0,0 +1,20 @@
+// MolarMass.cpp
+// This program should loop through the csv file Atomic Masses.csv and sum the masses of elements
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+int main()
+{
+	ifstream in("AtomicMasses.cpp"); //In an ideal world this opens file
+	double runningSum; 
+
+	for () //For each element in the Smile, this will see if it is present in the csv file
+		getline()
+		/* I need to find a way to deal with the two letter abbreviations of elements in the search function
+		   maybe something along the lines of "if [next value] is lowercase, then search both letters"*/
+
+
+	return 0;
+}
