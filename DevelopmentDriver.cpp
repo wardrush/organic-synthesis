207ba3be8cabb6254c83e9281922cdce1bb82b0f
Michael Whitehurst <Gawquon@users.noreply.github.com>
2017-01-11 21:32:50 -0600

    Update DevelopmentDriver.cpp

--- DevelopmentDriver.cpp
+++ DevelopmentDriver.cpp
@@ -20,15 +20,34 @@ std::vector<Branch> filterBranching(std::string molecule);
 int main()
 {
     std::vector<Branch> branches;
-    branches = filterBranching("");
+    branches = filterBranching("CC(CCC(C)=O)CCC(C)C(C(C)C)C");
 
     for(int i = 0; i < branches.size(); i++)
-        std::cout << branches[i].branchSMILES << ", " << branches[i].branchPoint << "/n";
+        std::cout << branches[i].branchSMILES << ", " << branches[i].branchPoint << "\n";
 }
 
 std::vector<Branch> filterBranching(std::string molecule)
 {
-    std::vector tmp;
-    //do things
+    std::vector<Branch> tmp;
+    //your code here
+
+    //Hint#1
+    tmp.push_back(Branch()); //adds a Branch struct to the end of the vector
+
+    //Hint#2
+    tmp[0].branchPoint = -1; // tmp[0] is accessing the vector tmp at the index of 0,
+    // aka the starting index. the period is accessing the scope of the struct and both
+    // branchPoint and branchSMILES can be accessed and mutated like any other variable in this way
+    tmp[0].branchSMILES = "CC(=O)C";
+
+    //Hint#3
+    std::string dummy = (molecule.substr(3,8)); // the substr function returns a string where
+    // the first value is the starting index in the string and the second value is the number
+    // of characters the new string spans
+    std::cout << dummy << std::endl;
+    std::cout << dummy.erase(2,4) << std::endl;
+    dummy += "=O";
+    std::cout << dummy << std::endl;
+    // look at http://www.cplusplus.com/reference/string/string/ for more functions
     return tmp;
 }
