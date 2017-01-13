3cad8429f92cdacdc5ec939e022c757e183dbdb0
Michael Whitehurst <Gawquon@users.noreply.github.com>
2017-01-13 01:45:57 -0600

    Update Molecule.h

--- Molecule.h
+++ Molecule.h
@@ -14,23 +14,28 @@
 class Molecule
 {
 private:
+    struct Branch
+    {
+        std::string branchSMILES; // The SMILES representation of the branch
+        int branchPoint; // index of the point where the branch connects to the parent branch
+        // value is -1 if parent branch
+    };
     friend std::ostream& operator<<(std::ostream&, const Molecule&);
     std::string SMILES;
     std::vector<Atom> atomList;
     BondList bonds;
-    bool isOrganic(std::string a) {
-        return false;
-    }
+    bool isOrganic(std::string a);
+    std::vector<Branch> filterBranching(std::string molecule);
+    void fillInHydrogens();
+    void convertToMolecule(std::string SMILES);
+    void simpleSMLIESconverter(std::string simpleSMILES);
+    bool isSimple(std::string simpleSMILES);
 
-    bool isAtom(std::string a) {
-        return false;
-    }
 
 public:
     Molecule();
     Molecule(std::string s);
     double calculateMolecularWeight();
-
 };
 
 inline std::ostream& operator<<(std::ostream &strm, const Molecule &m)
